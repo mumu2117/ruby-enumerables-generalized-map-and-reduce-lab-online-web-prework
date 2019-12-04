c6e81af61bd836d469e2df0006705243b7e0e3e4
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-04 20:34:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,32 @@
-# Your Code Here
+def map(source_array)
+  new = source_array
+  i=0 
+
+  while i < new.length do
+    new[i] = (yield(new[i]))
+
+    i += 1
+  end 
+
+  new
+end
+
+
+def reduce(source_array, starting_value = nil)
+  if starting_value == nil
+    value = starting_value
+    i=0
+  else
+    value = source_array[0]
+    i=1
+  end 
+
+
+  while i < source_array.length do 
+    value += source_array[i]
+    yield()
+    i += 1
+  end
+  #yield(value)
+  value
+end 
