a2e877698164d1f69b3735dd3e200d4bd370697c
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-04 20:36:44 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+def map(source_array)
+  return_array = []
+
+  source_array.each do |element|
+    return_array.push(yield(element))
+  end
+
+  return return_array
+end
+
+
+
+def reduce(source_array, starting_point = 0)
+  if(source_array[0].is_a? Numeric)
+    result = 0 + starting_point
+  else
+    result = false
+  end
+
+  source_array.each do |element|
+    result = yield(result, element)
+  end
+
+  return result
+end 
